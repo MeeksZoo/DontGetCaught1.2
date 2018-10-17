c094948d34738ad57438e67d7ecce4b5718e613e
Jenna <jennasmeeks@gmail.com>
2018-10-16 22:15:24 -0600

    Working on Getting a Sphere to Move

---
+++ DontGetCaught/DontGetCaught/Assets/Scripts/AddColor.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class AddColor : MonoBehaviour
+{
+
+	public UnityEvent Paint;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		Paint.Invoke();
+
+	}
+
+}
