78d76f107180f149edbd650decc04883f3512206
Jenna <jennasmeeks@gmail.com>
2018-10-18 21:20:42 -0600

    Working before and in class

---
+++ DontGetCaught/DontGetCaught/Assets/Scripts/UnityActionLight/LightAction.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events
+
+                 ;public class LightAction : MonoBehaviour
+{
+
+    public UnityAction LightSwitch;
+
+
+
+    void Start()
+    {
+
+        LightSwitch = LightOn;
+
+    }
+
+    private void OnMouseDown()
+    {
+        LightSwitch();
+    }
+
+    private void LightOn()
+    {
+        Debug.Log("on");
+        LightSwitch = LightOff;
+    }
+
+    private void LightOff()
+    {
+        Debug.Log("off");
+        LightSwitch = LightOn;
+    }
+}
