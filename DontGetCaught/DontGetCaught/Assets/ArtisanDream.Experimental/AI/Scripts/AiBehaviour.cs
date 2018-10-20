cdd4e73927e76f194f143371cbfaed4a803aee20
Jenna <jennasmeeks@gmail.com>
2018-10-19 18:03:39 -0600

    Adding AI Set

---
+++ DontGetCaught/DontGetCaught/Assets/ArtisanDream.Experimental/AI/Scripts/AiBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+//Made By Anthony Romrell
+public class AiBehaviour : MonoBehaviour
+{
+	private NavMeshAgent agent;
+	public AiBrain Brain;
+	public AiBase OnStart, OnEnter, OnExit;
+	private Coroutine coroutine;
+	public UnityEvent Event;
+	public GameAction TransferCoroutine;
+	
+	void Awake ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Start()
+	{
+		TransferCoroutine.CallNoArgs = CallNoArgs;
+		Brain.Base = OnStart;
+		coroutine = StartCoroutine(Brain.Base.Nav(agent));
+	}
+
+	private void CallNoArgs()
+	{
+		OnCall(coroutine);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Brain.Base = OnEnter;
+		OnCall(coroutine);
+	}
+	
+	private void OnTriggerExit(Collider other)
+	{
+		Brain.Base = OnExit;
+		OnCall(coroutine);
+	}
+
+	
+	
+	private void OnCall(Coroutine c)
+	{
+		StopCoroutine(c);
+		coroutine = StartCoroutine(Brain.Base.Nav(agent));
+	}
+
+}
---
+++ DontGetCaught/DontGetCaught/Assets/ArtisanDream.Tools/SingleVariables/FloatBase.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+
+	[CreateAssetMenu(fileName = "FloatBase")]
+	public class FloatBase : ScriptableObject
+	{
+		public float Value;
+	}
