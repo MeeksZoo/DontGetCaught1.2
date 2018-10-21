9d529fd70f2e36d1db83d48d2c50f3449b15da8c
Jenna <jennasmeeks@gmail.com>
2018-10-20 22:08:26 -0600

    Updating Code and getting things going

---
+++ DontGetCaught/DontGetCaught/Assets/AIProgram/FloatData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+//Made By Anthony Romrell
+
+[CreateAssetMenu(menuName = "Single Variables/FloatData")]
+public class FloatData : ScriptableObject
+{
+    [SerializeField] protected float value;
+
+    public virtual float Value
+    {
+        get => this.value;
+        set => this.value = value;
+    }
+}
---
+++ DontGetCaught/DontGetCaught/Assets/Scripts/CharacterMovers/CharacterMover.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class CharacterMover<Float> : MonoBehaviour 
+{
+
+    public CharacterController Controller;
+    private Vector3 position;
+	public Float Speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        position.x = Speed * Input.GetAxis("horizontal");
+		Controller.Move(position * Time.deltaTime);
+
+	}
+}
