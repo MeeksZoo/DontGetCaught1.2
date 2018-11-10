ffb8f93fe47e9a85e177b0a9fb1f094963f0ec1c
Jenna <jennasmeeks@gmail.com>
2018-11-10 14:38:24 -0700

    Finally The Character is Moving Almost the AI

---
+++ DontGetCaught/DontGetCaught/Assets/Scripts/CharacterMovers/MoveOnMouseDown.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveOnMouseDown : MonoBehaviour {
+
+    NavMeshAgent Movee;
+
+    void Start()
+    {
+        Movee = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                Movee.destination = hit.point;
+            }
+        }
+    }
+}
